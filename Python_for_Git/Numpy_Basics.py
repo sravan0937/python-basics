dacb06c2ca27d9ab154b406241ea780a15ae7103
Srav's <sravs@fakeemail.com>
2025-07-20 14:27:59 +0530

    3x3 simmple array

--- Python_for_Git/Numpy_Basics.py
+++ Python_for_Git/Numpy_Basics.py
@@ -1,4 +1,9 @@
 import numpy as np
-a= np.arange(0,10)
-
-print(a)
+#create a 3x3 array with values from 1 to 9
+array=np.array([[1, 2, 3],
+                [4, 5, 6],
+                [7, 8, 9]]) 
+print(f"Original Array:\n{array}")
+print(f"Shape of the array: {array.shape}")
+print(f"dimensions of the array: {array.ndim}")
+print(f"size of the array: {array.size}")
